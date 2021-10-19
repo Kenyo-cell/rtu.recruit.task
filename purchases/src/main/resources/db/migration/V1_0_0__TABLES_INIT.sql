08deb57a11d0b44735a57c8391c3125552fb2e45
Kenyo-cell <rekurt3000@gmail.com>
2021-10-20 00:19:03 +0300

    feat: set up flyway migration tool

---
+++ purchases/src/main/resources/db/migration/V1_0_0__TABLES_INIT.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS purchases (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    date DATE NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    total_cost DOUBLE NOT NULL,
+    CONSTRAINT user_id_fk
+    FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
